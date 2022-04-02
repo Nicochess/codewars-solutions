5945230cb4bbb67c732ddda1e3b72d8a8e7ec1ee
Nicolas Magno <66505477+Nicochess@users.noreply.github.com>
2022-04-02 20:38:15 -0300

    feat: new solution

--- javascript/11-countOdd.js
+++ javascript/11-countOdd.js
@@ -1,10 +1,3 @@
 function oddCount(n){
-    let oddNums = 0;
-    for(let i = 0; i < n; i++){
-      if(i % 2 !== 0){
-        oddNums += 1
-      }
-    }
-    
-    return oddNums
+    return Math.floor(n / 2)
 }
